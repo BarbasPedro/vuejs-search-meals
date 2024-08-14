161fad1f789f0d256d57c6f1ecdf9e01ba6308d8
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-08-14 13:36:44 -0300

    Creating actions

---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+export function searchMeals ({commit}, keyword) {
+  axiosClient.get(`search.php?s=${keyword}`)
+    .then(({data}) => {
+      debugger;
+      commit('setSearchedMeals', data)
+    })
+}
