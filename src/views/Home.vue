1a8dd43d6ba16f6fa1357cbeddee9429d6afb048
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-08-14 13:35:40 -0300

    Creating Views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="flex flex-col p-8">
-    <div class="items-center justify-center">
-      <input
-        type="text"
-        class="rounded border-2 border-gray-200 w-full"
-        placeholder="Search for Meals"
-      />
-    </div>
-
     <div class="flex justify-center gap-2 mt-2">
       <router-link
         :to="{name: 'byLetter', params: {letter}}"
@@ -16,14 +8,23 @@
       >
         {{ letter }}
       </router-link>
+
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient";
 
 const meals = computed(() => store.state.meals);
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ingredients = ref([])
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+  ingredients.value = response.data
+})
 </script>
